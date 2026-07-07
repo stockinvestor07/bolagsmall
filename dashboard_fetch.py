659c6512b42c0bbe11d53162dd85d86d7aef50ca
stockinvestor07 <anton.js.werner@gmail.com>
2026-07-08 00:05:22 +0200

    Add pandas import to dashboard_fetch.py

--- dashboard_fetch.py
+++ dashboard_fetch.py
@@ -14,6 +14,7 @@ import json
 import requests
 import yfinance as yf
 import gspread
+import pandas as pd
 from google.oauth2.service_account import Credentials
 from datetime import datetime
 from scipy.stats import percentileofscore
